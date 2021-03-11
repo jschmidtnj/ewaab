6c8fbacc014bdf67e3693d70a3e728121de8d8e1
Joshua <jschmidtnj@gmail.com>
2021-03-11 12:17:10 -0500

    lambda: adding global config file
    Signed-off-by: Joshua <jschmidtnj@gmail.com>

--- scripts/build_zip_lambda.sh
+++ scripts/build_zip_lambda.sh
@@ -13,6 +13,7 @@ rm -rf "$dist_folder" "$output"
 mkdir "$dist_folder"
 cp package.json "$dist_folder"
 cp yarn.lock "$dist_folder"
+cp *.yml "$dist_folder"
 cp -R lib/* "$dist_folder"
 
 cd "$dist_folder"
